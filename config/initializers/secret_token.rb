6e78a110cf01b973b1717b00eeb5e61ca0ba029a
Matthew Dyer <matthew.b.dyer@gmail.com>
2013-07-12 17:20:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slushpile::Application.config.secret_token = 'ec9fb5c7b450e690e751057da624e1b6577252cdca3b6bb219b67b619b3a4c0897d127f59dd56501e7cb12cc92109c93cd55975b4a5bf7e0277d2a063c565096'
